01712ed3d6473e984e28b5a7f68f58a74e1e6278
Sylvain Ni <sylvainni@msn.com>
2023-12-22 17:02:15 +0100

    Update to 21

--- lesson_3/twenty_one/twentyone.rb
+++ lesson_3/twenty_one/twentyone.rb
@@ -2,9 +2,8 @@
 require 'pry'
 
 CARD_VALUE = {
-  "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9, "10" => 10,
-  "jack" => 10, "queen" => 10, "king" => 10,
-  "ace" => 11
+  "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8,
+  "9" => 9, "10" => 10, "jack" => 10, "queen" => 10, "king" => 10, "ace" => 11
 }
 
 def prompt(msg)
@@ -23,38 +22,45 @@ def initialize_deck
   new_deck
 end
 
+# rubocop:disable Metrics/methodLength, Metrics/AbcSize
 def display_board(dealer_deck, dealer_score, player_deck, player_score, hide: false)
   system 'clear'
-  puts '21 (light version)'.center(30)
+  puts '21 (light version)'.center(26)
   puts ''
-  puts 'SCORE BOARD'
-  puts "DEALER: #{dealer_score}"
-  puts "PLAYER: #{player_score}"
+  puts 'SCORE BOARD'.center(26)
+  puts "DEALER: #{dealer_score}".center(26)
+  puts "PLAYER: #{player_score}".center(26)
   puts ''
   puts ''
-  puts 'DEALER DECK'.center(23)
-  puts "deck value : #{update_hand_value(dealer_deck, hide: true)}".center(23) # ajouter cas non-hide
-  puts display_deck(dealer_deck, hide: true)
+  puts 'DEALER DECK'.center(26)
+  if hide
+    puts "deck value : #{update_hand_value(dealer_deck, hide: true)}".center(26)
+    puts display_deck(dealer_deck, hide: true)
+  else
+    puts "deck value : #{update_hand_value(dealer_deck)}".center(26)
+    puts display_deck(dealer_deck)
+  end
   puts ''
   puts ''
   puts display_deck(player_deck)
-  puts "deck value : #{update_hand_value(player_deck)}".center(23)
-  puts 'PLAYER DECK'.center(23)
+  puts "deck value : #{update_hand_value(player_deck)}".center(26)
+  puts 'PLAYER DECK'.center(26)
   puts ''
 end
+# rubocop:enable Metrics/methodLength, Metrics/AbcSize
 
 def display_deck(table_deck, hide: false)
-  line_a = '+----------' * table_deck.size
-  line_b = '|          ' * table_deck.size
+  line_a = '+-----------' * table_deck.size
+  line_b = '|           ' * table_deck.size
   line1 = ''
   line2 = ''
   table_deck.each_with_index do |card, index|
     if hide && index > 0
-      line1 += "| #{'hidden'.center(9)}"
-      line2 += "| #{' '.center(9)}"
+      line1 += "| #{'hidden'.center(10)}"
+      line2 += "| #{' '.center(10)}"
     else
-      line1 += "| #{card[0].center(9)}"
-      line2 += "| #{card[1].center(9)}"
+      line1 += "| #{card[0].capitalize.center(10)}"
+      line2 += "| #{card[1].capitalize.center(10)}"
     end
   end
   ["#{line_a}+", "#{line_b}|", "#{line_b}|", "#{line1}|", "#{line_b}|",
@@ -84,6 +90,13 @@ def update_hand_value(table_deck, hide: false)
   result
 end
 
+def initial_card_distribution(deck, dealer_deck, player_deck)
+  1.upto(4) do |index|
+    player_deck << distribute_card!(deck) if index.even?
+    dealer_deck << distribute_card!(deck) if index.odd?
+  end
+end
+
 def distribute_card!(deck)
   deck.delete(deck.sample)
 end
@@ -104,52 +117,69 @@ def player_deck_update(player_deck, deck)
   player_deck
 end
 
-deck = initialize_deck
-player_deck = []
-player_score = 0
-dealer_deck = []
-dealer_score = 0
-bust = false
-
-# initial round
-1.upto(4) do |index|
-  player_deck << distribute_card!(deck) if index.even?
-  dealer_deck << distribute_card!(deck) if index.odd?
+def round_score_update(dealer_hand_value, player_hand_value, dealer_score, player_score)
+  if dealer_hand_value <= 21 && dealer_hand_value > player_hand_value
+    prompt "DEALER wins this round."
+    dealer_score += 1
+  elsif dealer_hand_value > 21 || player_hand_value > dealer_hand_value
+    prompt "PLAYER wins this round!"
+    player_score += 1
+  elsif player_hand_value == dealer_hand_value
+    prompt "It's a draw!"
+  end
+  [dealer_score, player_score]
 end
 
-# player_deck = [["", "2"], ["", "2"], ["", "ace"], ["", "ace"], ["", "ace"], ["", "ace"]]
-# p update_hand_value(player_deck)
-# puts display_deck(player_deck, hide: true)
-# puts display_deck(player_deck)
-
-# puts display_deck(player_deck)
-# p player_deck
-# p dealer_deck
-# p deck
+# START
+dealer_score = 0
+player_score = 0
 
 loop do
-  display_board(dealer_deck, dealer_score, player_deck, player_score, hide: true)
+  deck = initialize_deck
+  dealer_deck = []
+  player_deck = []
 
-  hand_value = update_hand_value(player_deck)
-  if hand_value > 21
-    bust = true
-    prompt "BUST. You are over 21"
-    prompt "DEALER wins this round"
-    dealer_score += 1
-    break
+  initial_card_distribution(deck, dealer_deck, player_deck)
+  # binding.pry
+
+  loop do
+    player_hand_value = update_hand_value(player_deck)
+    display_board(dealer_deck, dealer_score, player_deck, player_score, hide: true)
+
+    if player_hand_value == 21
+      prompt "PLAYER wins this round!"
+      player_score += 1
+      break
+    elsif player_hand_value > 21
+      prompt "BUST! You are over 21."
+      prompt "DEALER wins this round."
+      dealer_score += 1
+      break
+    end
+
+    choice = player_choice
+    if choice == 1
+      player_deck = player_deck_update(player_deck, deck)
+    elsif choice == 2
+      dealer_hand_value = 0
+      loop do
+        prompt "DEALER's move."
+        sleep 1
+        dealer_hand_value = update_hand_value(dealer_deck)
+        display_board(dealer_deck, dealer_score, player_deck, player_score)
+        break if dealer_hand_value >= 17
+        dealer_deck = player_deck_update(dealer_deck, deck) if dealer_hand_value < 17
+      end
+
+      display_board(dealer_deck, dealer_score, player_deck, player_score)
+      dealer_score, player_score = round_score_update(dealer_hand_value, player_hand_value,
+                                                      dealer_score, player_score)
+      break
+    end
   end
 
-  choice = player_choice
-  if choice == 1
-    player_deck = player_deck_update(player_deck, deck)
-  elsif choice == 2
-    break
-  end
-
-  # loop do
-
-
-  # end
+  prompt "Press any key to continue"
+  gets.chomp
 end
 
-# display_board(dealer_deck, dealer_score, player_deck, player_score, hide: false)
+display_board(dealer_deck, dealer_score, player_deck, player_score, hide: false)
